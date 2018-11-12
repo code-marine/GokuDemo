4f7f908518d37ca250b322981f4cfb2f84e2c7bd
shivamdawar97 <shivam97.dawar@gmail.com>
2018-11-12 12:08:38 +0530

    project initialized

---
+++ app/src/main/java/com/contri/WelcomActivity.kt
@@ -0,0 +1,12 @@
+package com.contri
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class WelcomActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.a_welcom)
+    }
+}
